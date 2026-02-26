04df0ec23fb4e1b5e39de9da94ebd548d9f11da0
OsirisHP <Usir95.hp@gmail.com>
2026-02-26 13:46:58 -0600

    Icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="icon" href="{{ asset('img/Icon.png') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&family=Patrick+Hand&family=Caveat:wght@400;500;600;700&family=Open+Sans:wght@300;400;500;600;700&family=Nunito:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
 
         <!-- Scripts -->
         @routes
